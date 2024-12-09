7dc91055681944ef50d939fcceec0a7eb8d77c23
BenBrooke450 <bensir@mail.com>
2024-12-09 18:05:45 +0000

    Basic

---
+++ Leetcode/459. Repeated Substring Pattern.py
@@ -0,0 +1,44 @@
+"""
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+
+
+"""
+
+def func(string:str):
+
+    length = len(string)
+
+    for x in range(1,length):
+
+        if length % x == 0:
+
+            substring = string[: x]
+
+            repeated_substring = substring * ( length // x )
+
+            if repeated_substring == string:
+                return True
+
+    return False
+
+
+
+func("abcabcabcabc")
+
