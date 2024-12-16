293e580f98b068681782ad3d7c65ef1ebd1264b8
BenBrooke450 <bensir@mail.com>
2024-12-16 20:22:11 +0000

    Basic

---
+++ Leetcode/2652. Sum Multiples.py
@@ -0,0 +1,45 @@
+
+
+"""
+Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+
+Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+
+
+
+Example 1:
+
+Input: n = 7
+Output: 21
+Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
+Example 2:
+
+Input: n = 10
+Output: 40
+Explanation: Numbers in the range [1, 10] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9, 10. The sum of these numbers is 40.
+Example 3:
+
+Input: n = 9
+Output: 30
+Explanation: Numbers in the range [1, 9] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9. The sum of these numbers is 30.
+
+
+"""
+
+
+def bigrange(number:int):
+
+    list3 = []
+
+    for nums in range(0,number+1):
+        if nums % 3 == 0 or nums % 5 == 0 or nums % 7 ==0:
+
+            list3.append(nums)
+
+
+    return sum(list3)
+
+
+print(bigrange(9))
+
+
