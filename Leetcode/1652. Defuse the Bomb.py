edd4e0be2adfce39326dff7a05c06748b361ed04
BenBrooke450 <bensir@mail.com>
2025-01-07 20:23:49 +0000

    Basic

---
+++ Leetcode/1652. Defuse the Bomb.py
@@ -0,0 +1,79 @@
+
+"""
+You have a bomb to defuse, and your time is running out!
+    Your informer will provide you with a circular array code of length of n and a key k.
+
+To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+
+If k > 0, replace the ith number with the sum of the next k numbers.
+If k < 0, replace the ith number with the sum of the previous k numbers.
+If k == 0, replace the ith number with 0.
+As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+
+Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+
+
+
+Example 1:
+Input: code = [5,7,1,4], k = 3
+Output: [12,10,16,13]
+Explanation: Each number is replaced by the sum of the next 3 numbers.
+    The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. Notice that the numbers wrap around.
+
+
+Example 2:
+Input: code = [1,2,3,4], k = 0
+Output: [0,0,0,0]
+Explanation: When k is zero, the numbers are replaced by 0.
+
+Example 3:
+Input: code = [2,4,9,3], k = -2
+Output: [12,5,6,13]
+Explanation: The decrypted code is [3+9, 2+3, 4+2, 9+4].
+    Notice that the numbers wrap around again. If k is negative, the sum is of the previous numbers.
+"""
+
+
+def decrypt(code: list[int], k: int):
+
+    long_code = code*(k+1)
+    long_code_negative = code[::-1]*abs(k-1)
+    i = 0
+    while i < len(code):
+        print(code[i+1:i+k+1])
+        if k >= 0:
+            code[i] = sum(long_code[i+1:i+k+1])
+            i = i + 1
+        elif k < 0:
+            code[i] = sum(long_code_negative[i + 1:i + abs(k) + 1])
+            i = i + 1
+
+    if k >= 0:
+        return code
+    else:
+        return code[::-1]
+
+
+print(decrypt([5,7,1,4],3))
+#[12,10,16,13]
+
+print(decrypt([1,2,3,4],0))
+#[0,0,0,0]
+
+print(decrypt([2,4,9,3],-2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
