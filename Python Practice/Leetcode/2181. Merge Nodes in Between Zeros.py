60ec2a2fa5ed9b098152dfb6fa9d6845f4b024bf
BenBrooke450 <bensir@mail.com>
2025-06-01 11:30:54 +0200

    Basic

---
+++ Python Practice/Leetcode/2181. Merge Nodes in Between Zeros.py
@@ -0,0 +1,21 @@
+
+
+
+class Solution:
+    def mergeNodes(self, head: Optional[listNode]) -> Optional[ListNode]:
+        dummy = listNode(0)  # Temporary start node
+        tail = dummy
+        total = 0
+        curr = head
+        first = 1
+        while curr:
+            if curr.val == 0 and first != 1:
+                tail.next = ListNode(total)  # Copy the node
+                tail = tail.next
+                total = 0
+            else:
+                total = total + curr.val
+            curr = curr.next
+            first = 0
+
+        return dummy.next
