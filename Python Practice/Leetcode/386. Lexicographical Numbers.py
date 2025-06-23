f27a67ad6b48aa644171782bd974d046acbf64ce
BenBrooke450 <bensir@mail.com>
2025-06-23 21:52:38 +0200

    Basic

---
+++ Python Practice/Leetcode/386. Lexicographical Numbers.py
@@ -0,0 +1,33 @@
+
+"""
+Given an integer n, return all the numbers in the range
+ [1, n] sorted in lexicographical order.
+
+You must write an algorithm that runs in O(n) time and uses O(1) extra space.
+
+
+
+Example 1:
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+
+Example 2:
+Input: n = 2
+Output: [1,2]
+
+"""
+
+import numpy as np
+def lexicalOrder(n: int) -> list[int]:
+    t = ["1","2","3","4","5","6","7","8","9"]
+    i = 0
+    array_n = np.arange(1,n+1)
+
+    while True:
+        array_n.sort()
+
+
+
+
+
+
