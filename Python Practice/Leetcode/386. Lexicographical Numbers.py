d9488c2fdcbf00fd376d8e6479229bcb674f507c
BenBrooke450 <bensir@mail.com>
2025-06-23 23:06:47 +0200

    Basic

--- Python Practice/Leetcode/386. Lexicographical Numbers.py
+++ Python Practice/Leetcode/386. Lexicographical Numbers.py
@@ -17,6 +17,28 @@ Output: [1,2]
 
 """
 
+
+
+def lexicalOrder(n: int) -> list[int]:
+
+    res = []
+
+    def dfs(cur):
+        if cur > n:
+            return
+        res.append(cur)
+        for i in range(10):
+            dfs(cur * 10 + i)
+
+    for i in range(1,10):
+        dfs(i)
+    return res
+
+print(lexicalOrder(136))
+
+
+
+
 import numpy as np
 def lexicalOrder(n: int) -> list[int]:
     t = ["1","2","3","4","5","6","7","8","9"]
