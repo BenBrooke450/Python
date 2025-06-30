1f8cfd4fe440ab0482aabaf4ca773355b510ac27
BenBrooke450 <bensir@mail.com>
2025-06-30 18:36:42 +0200

    Basic

--- Python Practice/Leetcode/118. Pascal's Triangle.py
+++ Python Practice/Leetcode/118. Pascal's Triangle.py
@@ -31,6 +31,49 @@ Constraints:
 """
 
 
+def generate(numRows: int) -> list[list[int]]:
+
+    list1 = []
+
+    def pas(first,incre,numRows):
+
+        list1.append(first[:])
+        first.append(incre)
+        first = [1] + [sum(first[x:x+2]) for x in range(len(first)-1)]
+        if len(list1) >= numRows:
+            return list1
+        else:
+            pas(first,incre,numRows)
+
+    pas(first = [1],incre = 0,numRows = numRows)
+
+    return list1
+
+
+print(generate(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 
 def pas(num : int):
 
@@ -126,3 +169,4 @@ print(list2)
 print([[x, x + 1] for x in range(10)])
 #[[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]]
 
+"""
