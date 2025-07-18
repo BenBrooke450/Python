3ee4edc6436dae894545277e95e6fc30a1ee5b63
BenBrooke450 <bensir@mail.com>
2025-07-18 23:11:48 +0200

    basic

--- Python Practice/Leetcode/1072. Flip Columns For Maximum Number of Equal Rows.py
+++ Python Practice/Leetcode/1072. Flip Columns For Maximum Number of Equal Rows.py
@@ -27,30 +27,125 @@ Explanation: After flipping values in the first two columns, the last two rows h
 
 """
 
+
 import numpy as np
+
 def maxEqualRowsAfterFlips(matrix: list[list[int]]) -> int:
     matrix = np.array(matrix)
-
-    print(matrix[:,:1])
-    print(matrix.shape[0])
+    list1 = []
     t = 0
+    p = 0
+    for i in range(matrix.shape[0]):
+        row = matrix[i, :]
+        list1.append([])  # Initialize a new list for each row
+        current_segment = [row[0]]
 
-    for y in range(matrix.shape[1]):
-        print(matrix[:, y:y+1])
+        for j in range(1, len(row)):
+            if row[j] == current_segment[0]:
+                current_segment.append(row[j])
+            else:
+                list1[i].append(current_segment)
+                current_segment = [row[j]]
 
-        for x in range(matrix.shape[0]):
-            print("shape:",matrix[x:x+1,:][0])
-            s = sum(matrix[x:x+1,:][0])
-            if s == 1 or s == 3:
-                t = t + 1
+        if current_segment:
+            list1[i].append(current_segment)
 
-    return t
+    return list1
+
 
 #print(maxEqualRowsAfterFlips(matrix = [[0,1],[1,1]]))
 
 #print(maxEqualRowsAfterFlips(matrix = [[0,1],[1,0]]))
 
 print(maxEqualRowsAfterFlips(matrix = [[0,0,0],[0,0,1],[1,1,0]]))
+
+
+#######################
+
+
+import numpy as np
+
+def maxEqualRowsAfterFlips(matrix: list[list[int]]) -> int:
+    matrix = np.array(matrix)
+    list1 = []
+    t = 0
+    p = 0
+    for i in range(matrix.shape[0]):
+        row = matrix[i, :]
+        print(row)
+        list1.append([])  # Initialize a new list for each row
+
+        current_segment = [row[0]]
+
+        for j in range(1, len(row)):
+            if row[j] == current_segment[0]:
+                current_segment.append(row[j])
+            else:
+                list1[i].append(current_segment)
+                current_segment = [row[j]]
+
+        if current_segment:
+            list1[i].append(current_segment)
+
+    return list1
+
+
+#print(maxEqualRowsAfterFlips(matrix = [[0,1],[1,1]]))
+
+#print(maxEqualRowsAfterFlips(matrix = [[0,1],[1,0]]))
+
+print(maxEqualRowsAfterFlips(matrix = [[0,0,0],[0,0,1],[1,1,0]]))
+
+
+
+#######################################################
+
+import numpy as np
+def maxEqualRowsAfterFlips(matrix: list[list[int]]) -> int:
+    matrix = np.array(matrix)
+    list1 = [[] for x in range(matrix.shape[0])]
+    t = 0
+    p = 0
+    for i,x in enumerate(range(matrix.shape[0])):
+        print("shape:",matrix[x:x+1,:][0])
+        list2=[]
+        m = 0
+        n = 0
+        for j,y in enumerate(matrix[x:x+1,:][0]):
+            if y == 0:
+                if n == 0:
+                    list2.append(0)
+                    if j + 1 == len(matrix[x:x+1,:][0]):
+                        list1[i].append(list2)
+                else:
+                    list1[i].append(list2)
+                    list2 = []
+                    n = 0
+                    list2.append(0)
+                    if j + 1 == len(matrix[x:x+1,:][0]):
+                        list1[i].append(list2)
+                m = m + 1
+            else:
+                if m == 0:
+                    list2.append(1)
+                    if j + 1 == len(matrix[x:x+1,:][0]):
+                        list1[i].append(list2)
+                else:
+                    list1[i].append(list2)
+                    list2 = []
+                    m = 0
+                    list2.append(1)
+                    if j + 1 == len(matrix[x:x+1,:][0]):
+                        list1[i].append(list2)
+                n = n + 1
+    return list1
+
+
+#print(maxEqualRowsAfterFlips(matrix = [[0,1],[1,1]]))
+
+#print(maxEqualRowsAfterFlips(matrix = [[0,1],[1,0]]))
+
+#print(maxEqualRowsAfterFlips(matrix = [[0,0,0],[0,0,1],[1,1,0]]))
 
 
 
