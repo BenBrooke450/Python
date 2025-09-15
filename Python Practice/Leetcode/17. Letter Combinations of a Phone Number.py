3caf12f9198cfaa37cc1e84236424cb3373daecc
BenBrooke450 <bensir@mail.com>
2025-09-16 01:29:51 +0200

    basic

---
+++ Python Practice/Leetcode/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,66 @@
+
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = ""
+Output: []
+
+Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+
+"""
+
+from itertools import combinations,permutations
+
+
+
+def letterCombinations(digits: str) -> list[str]:
+
+    tele = {"2":['a', 'b', 'c'], "3":['d', 'e', 'f'], "4":['g', 'h', 'i'], "5":['j', 'k', 'l'], "6":['m', 'n', 'o'], "7":['p', 'q', 'r','s'], "8": ['t', 'u', 'v'], "9": ['w','x','y','z']}
+
+    list1 = []
+
+    for x in digits:
+        list1.append(tele[x])
+
+    print(list1)
+
+    list2 = []
+    start = list1[0]
+    i = 0
+
+    def loop(numbers : list[list[str]],list2,start:list[str],i):
+
+
+        for x in start:
+            for y in list1[1 + i]:
+                list2.append([x,y])
+
+        start = list2
+        list2 = []
+        print("start:  ",start)
+
+        if i + 2 < len(list1):
+            loop(list1,list2,start, i+1)
+
+        else:
+            return start
+
+    loop(list1,list2,start,i)
+
+    return start
+
+
+print(letterCombinations("2324"))
+
