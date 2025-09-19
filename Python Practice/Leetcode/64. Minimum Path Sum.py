d62db9799323bfe79b7ed4814f5f3e63dc5d4d60
BenBrooke450 <bensir@mail.com>
2025-09-20 00:57:04 +0200

    basic

---
+++ Python Practice/Leetcode/64. Minimum Path Sum.py
@@ -0,0 +1,49 @@
+
+"""
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+
+
+Example 1:
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+
+Example 2:
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+
+"""
+
+
+def minPathSum(grid: list[list[int]]) -> int:
+
+    cols = len(grid[0])
+    rows = len(grid)
+
+    def dfs(r, c):
+        if (r < 0 or r >= rows or c < 0 or c >= cols):
+            return
+
+        # Explore in all 4 directions
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+
+    count = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1 and (r, c) not in visited:
+                dfs(r, c)
+                count += 1
+
+
+
+
+
+
