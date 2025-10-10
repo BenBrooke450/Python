75c45824de56112e17400246daa92c8e35138070
BenBrooke450 <bensir@mail.com>
2025-10-11 00:17:15 +0200

    basic

--- Python Practice/Leetcode/131. Palindrome Partitioning.py
+++ Python Practice/Leetcode/131. Palindrome Partitioning.py
@@ -17,35 +17,55 @@ Output: [["a"]]
 """
 
 
-from itertools import combinations, permutations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def partition(s: str) -> list[list[str]]:
-
     list1 = []
 
     for x in range(len(s)):
-        for y in range(x+1,len(s)):
-            if s[x:y] == s[x:y][::-1] and len(s[x:y])>1:
-                if len(s[:x]) == 0:
-                    list1.append([s[x:y].split(",")[0]] + [x for x in s[y:]])
+        for y in range(x+1, len(s)+1):  # include last character
+            if s[x:y] == s[x:y][::-1] and len(s[x:y]) > 0:
+                if x == 0:
+                    list1.append([s[x:y]] + [ch for ch in s[y:]])
                 else:
-                    list1.append([x for x in s[:x]] + [s[x:y].split(",")[0]] + [x for x in s[y:]])
+                    list1.append([ch for ch in s[:x]] + [s[x:y]] + [ch for ch in s[y:]])
 
-    for q in list1:
+
+    for q in list1.copy():
         for x in range(len(q)):
-            for y in range(x + 1, len(q)):
-
-                if len(q[x]) > 1:
-                    break
-
-                if q[x:y] == q[x:y][::-1] and len(q[x:y]) > 1:
-                    if len(q[:x]) == 0:
-                        list1.append([q[x:y].split(",")[0]] + [x for x in q[y:]])
+            for y in range(x + 1, len(q)+1):
+                substring = ''.join(q[x:y])
+                if len(substring) > 1 and substring == substring[::-1]:
+                    if x == 0:
+                        list1.append([substring] + q[y:])
                     else:
-                        list1.append([x for x in q[:x]] + [q[x:y].split(",")[0]] + [x for x in q[y:]])
+                        list1.append(q[:x] + [substring] + q[y:])
+
+    list2 = []
+
+    for x in list1:
+        if x not in list2:
+            list2.append(x)
+
+    return list2
 
 
-    return list1
+
 
 
 print(partition(s = "aabaabba"))
---
+++ Python Practice/Leetcode/980. Unique Paths III.py
@@ -0,0 +1,143 @@
+
+"""
+You are given an m x n integer array grid where grid[i][j] could be:
+
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+
+
+Example 1:
+
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths:
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+Example 2:
+
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths:
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+Example 3:
+
+
+Input: grid = [[0,1],[2,0]]
+Output: 0
+Explanation: There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+
+"""
+
+
+
+
+def uniquePathsIII(grid: list[list[int]]) -> int:
+
+    visted = list()
+    col_b = len(grid[0])
+    row_b = len(grid)
+    full_walk = []
+    ones = sum(1 for x in grid for y in x if y == -1)
+
+    def dfs(r,c):
+        if (r,c) in visted or r < 0 or r >= row_b or c < 0 or c >= col_b or grid[r][c] == -1:
+            print("out: ",(r, c))
+            return
+
+        visted.append((r,c))
+
+        print((r,c))
+
+        if grid[r][c] == 2 and len(visted) == col_b * row_b - ones:
+            print("got it",(r,c))
+            full_walk.append(visted[:])
+
+        else:
+            dfs(r,c+1)
+            print((r, c))
+            dfs(r+1,c)
+            print((r, c))
+            dfs(r,c-1)
+            print((r, c))
+            dfs(r-1,c)
+
+        visted.pop()
+
+
+    x = [[i,j] for i,x in enumerate(grid) for j,y in enumerate(x) if y ==1]
+    r = x[0][0]
+    c = x[0][1]
+    dfs(r,c)
+
+    print(full_walk)
+
+    return len(full_walk)
+
+
+#print(uniquePathsIII([[1,0,0,0],[0,0,0,0],[0,0,2,-1]]))
+print(uniquePathsIII([[0,1],[2,0]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def uniquePathsIII(grid: list[list[int]]) -> int:
+
+    visted = set()
+    col_b = len(grid[0])
+    row_b = len(grid)
+    full_walk = []
+    ones = sum(1 for x in grid for y in x if y == -1)
+
+    def dfs(r,c):
+        if (r,c) in visted or r < 0 or r >= row_b or c < 0 or c >= col_b or grid[r][c] == -1 or len(visted) == col_b * row_b - ones:
+            print("out: ",(r, c))
+            return
+
+        visted.add((r,c))
+
+        print((r,c))
+
+        if grid[r][c] == 2:
+            full_walk.append(visted)
+            return
+
+        dfs(r,c+1)
+        print((r, c))
+        dfs(r+1,c)
+        print((r, c))
+        dfs(r,c-1)
+        print((r, c))
+        dfs(r-1,c)
+
+
+    dfs(0,0)
+
+    return full_walk
+
+
+
