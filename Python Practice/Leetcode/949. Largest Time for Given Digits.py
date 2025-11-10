cd9ba4250c2804c5b3ad1773e94c0e04e929ee63
BenBrooke450 <bensir@mail.com>
2025-11-10 23:56:11 +0100

    basic

--- Python Practice/Leetcode/949. Largest Time for Given Digits.py
+++ Python Practice/Leetcode/949. Largest Time for Given Digits.py
@@ -23,3 +23,54 @@ Explanation: There are no valid 24-hour times as "55:55" is not valid.
 
 def largestTimeFromDigits(arr: list[int]) -> str:
 
+    max_list = []
+
+    for i,x in enumerate(arr):
+        for y in arr[i+1:]:
+            print(x,y)
+            x, y = str(x), str(y)
+            hour = int(x + y)
+            if hour < 24:
+                max_list.append(hour)
+
+    if len(max_list) != 0:
+        hour = str(max(max_list))
+    else:
+        hour = 0
+
+    print(hour)
+
+    for x in hour[0]:
+        i = arr.index(int(x))
+        arr.pop(i)
+
+    for x in hour[1]:
+        i = arr.index(int(x))
+        arr.pop(i)
+
+    print(arr)
+
+    max_minute = 0
+
+    one = int("".join([str(x) for x in arr]))
+    two = int("".join([str(x) for x in arr][::-1]))
+
+    print(one,two)
+
+    if one < 59 and two < 59:
+        max_minute = max(one,two)
+    elif one < 59:
+        max_minute = one
+    elif two < 59:
+        max_minute = two
+
+    if max_minute == 0 or hour == 0:
+        return ""
+
+    return str(hour) + ":" + str(max_minute)
+
+print(largestTimeFromDigits(arr = [1,2,3,4]))
+
+
+
+
