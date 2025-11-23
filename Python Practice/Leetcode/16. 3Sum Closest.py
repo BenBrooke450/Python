4c653eeec59fe22355ec245e3dd0da7cccb1a41a
BenBrooke450 <bensir@mail.com>
2025-11-23 01:01:21 +0100

    basic

--- Python Practice/Leetcode/16. 3Sum Closest.py
+++ Python Practice/Leetcode/16. 3Sum Closest.py
@@ -28,11 +28,17 @@ def threeSumClosest(nums: list[int], target: int) -> int:
     nums.sort()
     closest_sum = float('inf')
 
+    print(nums)
+
     for i in range(len(nums) - 2):
         left, right = i + 1, len(nums) - 1
 
         while left < right:
             current = nums[i] + nums[left] + nums[right]
+
+            print(nums[:i+1],nums[left],nums[right:])
+            print(nums[i], nums[left], nums[right])
+            print("  ")
 
             if abs(current - target) < abs(closest_sum - target):
                 closest_sum = current
@@ -43,77 +49,9 @@ def threeSumClosest(nums: list[int], target: int) -> int:
                 right -= 1
             else:
                 return current
+    return closest_sum
 
-print(threeSumClosest(nums = [-1,2,1,-4], target = 1))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def threeSumClosest(nums: list[int], target: int) -> int:
-
-    lowest_num = 10000
-    cloest_sum = 0
-
-    nums = tuple(nums)
-
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-
-            sum_nums = nums[i] + nums[j]
-
-            diff = sum_nums - target
-
-            if abs(diff) <= lowest_num:
-                lowest_num = abs(diff)
-                cloest_sum = sum_nums
-
-                if diff == 0:
-                    return abs(cloest_sum)
-
-    return abs(cloest_sum)
-
-
-
-
-
-
-
-
-
-
-
-
-
-from itertools import combinations, permutations
-def threeSumClosest(nums: list[int], target: int) -> int:
-
-
-    remainders = {abs(sum(x)-target):sum(x) for x in combinations(nums,3)}
-
-    min_remainders = min(remainders.keys())
-
-    remainders[min_remainders]
-
-    return remainders[min_remainders],{sum(x):abs(sum(x)-target) for x in combinations(nums,3)}
-
-
-
-
-print(threeSumClosest(nums = [0,-9,1,-4,3,13], target = -10))
-
+print(threeSumClosest(nums = [0,-9,1,-4,3,13,-15,-2], target = 100))
 
 
 
